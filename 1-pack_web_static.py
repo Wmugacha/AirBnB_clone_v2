830e79f6829ea597c60d90ddde5e860d46e8e67f
Wmugacha <wilfredmugacha@gmail.com>
2023-04-23 10:58:42 +0000

    compress web_static code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,15 +5,16 @@ from datetime import date
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents of web_static folder"""
+    """Function that generates a .tgz archive from the contents of the
+    web_static folder of your AirBnB Clone repo."""
 
-    filename = strftime("%Y%m%d%H%M%S")
+    archive_name = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
+              .format(archive_name))
 
-        return "versions/web_static_{}.tgz".format(filename)
+        return "versions/web_static_{}.tgz".format(archive_name)
 
     except Exception as e:
         return None
