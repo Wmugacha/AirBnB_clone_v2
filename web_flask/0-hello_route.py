89b4807109432da6204826c148368b39d17aad2a
Wmugacha <wilfredmugacha@gmail.com>
2023-05-20 07:10:54 +0000

    script to start web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+AirBnB Clone
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Start Flask """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    # python3 -m web_flask.0-hello_route
+    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
