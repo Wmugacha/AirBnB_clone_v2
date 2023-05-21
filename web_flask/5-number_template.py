81f34f8630d393ad1ce3643602b17de7be898931
Wmugacha <wilfredmugacha@gmail.com>
2023-05-21 07:34:46 +0000

    render template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+AirBnB Clone
+*** Web Framework with Flask ***
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function to display Hello hbnb """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_start():
+    """ Function to display hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_C(text):
+    """ Function to display C with a value variable """
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def display_python(text="is cool"):
+    """ Function to display Python with a value variable """
+    return 'Python ' + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """Function to display an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_int(n):
+    """Display HTML page only if n is an int"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    # python3 -m web_flask.2-c_route
+    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
