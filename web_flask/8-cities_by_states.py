a5575695251615d030b60607e1b5eafc88c3727d
Wmugacha <wilfredmugacha@gmail.com>
2023-05-21 15:55:42 +0000

    list cities from states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,24 +5,16 @@ Module to run an instance of AirBnB Clone with Flask
 
 from models import storage
 from models.state import State
-from models.city import City
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """ Method to list all states """
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def list_cities():
     """List all city Objects linked to a state"""
-    cities = storage.all(City)
-    return render_template('8-cities_by_states.html', cities=cities)
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
